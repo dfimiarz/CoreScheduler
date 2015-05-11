b72d1fa39db6b5f45e5aea46f8c9938e63e19062
dfimiarz <dfimiarz@ccny.cuny.edu>
2015-05-11 13:34:40 -0400

    Added page title and icon to the main template.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $rid = $utils->getRID();
 
 $view = new MainView();
 
-$arr_variables = ["rid" => $rid];
+$arr_variables = ["rid" => $rid,"page_title"=>"DivOfScience - Conference Room Reservations","icon"=>"./images/scidivicon.ico"];
 
 echo $view->render($arr_variables);
 
