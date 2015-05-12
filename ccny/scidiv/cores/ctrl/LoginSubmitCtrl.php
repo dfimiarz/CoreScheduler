74ee0296e41c299e36dbfb27df0982b03de38d19
dfimiarz <dfimiarz@ccny.cuny.edu>
2015-05-12 08:02:55 -0400

    Implementation of a redirect-after-post controller.

---
+++ ccny/scidiv/cores/ctrl/LoginSubmitCtrl.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Daniel F.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace ccny\scidiv\cores\ctrl;
+
+use Symfony\Component\HttpFoundation\Session\Session as Session;
+use Symfony\Component\HttpFoundation\Request as Request;
+use ccny\scidiv\cores\model\CoreUser as CoreUser;
+
+/**
+ * Description of LoginCtrl
+ *
+ * @author Daniel F
+ */
+class LoginSubmitCtrl extends RAPController {
+    
+    /* @var $request Request */
+    private $request;
+    
+    public function __construct() {
+        
+        parent::__construct();
+        $this->request = Request::createFromGlobals();
+        
+        
+    }
+    
+    public function run()
+    {
+        $username = $request->request->get("user",null);
+        $password = $request->request->get("pass",null);
+    }
+}
