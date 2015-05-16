3a1f6d55a751eeb4ee0fbc84fc9a9499e79e7fbe
dfimiarz <dfimiarz@ccny.cuny.edu>
2015-05-16 18:25:44 -0400

    Added new login page. This page is the first attempt to relay more on RAP controllers for actions that do not need AJAX calls.

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Daniel Fimiarz <dfimiarz@ccny.cuny.edu>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+include_once './ccny/scidiv/cores/autoloader.php';
+include_once './ccny/scidiv/cores/components/Utils.php';
+include_once './ccny/scidiv/cores/config/config.php';
+include_once './ccny/scidiv/cores/view/CoreView.php';
+
+use ccny\scidiv\cores\view\CoreView as CoreView;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+$session = new Session();
+$session->start();
+
+$request = Request::createFromGlobals();
+
+$error_txt = $session->get('login_err', null);
+$session->remove('login_err');
+
+$after_login_dest = $request->query->get('dest','');
+
+$view = new CoreView();
+$view->loadTemplate('login.html.twig');
+
+$arr_variables = ['destination'=>$after_login_dest,"error_txt"=>$error_txt,"page_title"=>"DivOfScience - Conference Room Reservations","icon"=>SYSTEM_ICON];
+
+echo $view->render($arr_variables);
+
